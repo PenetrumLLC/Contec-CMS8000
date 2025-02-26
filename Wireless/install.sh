a0cb91c756540aa681f8c4309345406346d85766
Ekultek <14183473+Ekultek@users.noreply.github.com>
2025-02-26 12:14:52 -0600

    extracted firmware

---
+++ Wireless/install.sh
@@ -0,0 +1,15 @@
+/sbin/insmod /opt/Wireless/8188eu.ko
+/sbin/insmod /opt/Wireless/rt3070sta.ko
+/sbin/insmod /opt/Wireless/8712u.ko
+/sbin/insmod /opt/Wireless/8192cu.ko
+/sbin/insmod /opt/Wireless/compat.ko
+/sbin/insmod /opt/Wireless/compat_firmware_class.ko
+/sbin/insmod /opt/Wireless/rfkill_backport.ko
+/sbin/insmod /opt/Wireless/cfg80211.ko
+/sbin/insmod /opt/Wireless/mac80211.ko
+/sbin/insmod /opt/Wireless/ath.ko
+/sbin/insmod /opt/Wireless/ath9k_hw.ko
+/sbin/insmod /opt/Wireless/ath9k_common.ko
+/sbin/insmod /opt/Wireless/ath9k.ko
+/sbin/insmod /opt/Wireless/ath9k_htc.ko
+
